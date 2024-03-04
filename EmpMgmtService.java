4ccff1abef4628f8a6583360e425973061c4bef1
Nalankumar <Nalankumar@users.noreply.github.com>
2024-03-04 21:12:42 +0530

    search by dept and removal of an employee have been developed

--- EmpMgmtService.java
+++ EmpMgmtService.java
@@ -121,5 +121,41 @@ public class EmpMgmtService {
             System.err.println(e);
         } 
     } 
-
+    public void searchByDepartment(String dept){
+        Database db = new Database();        
+       try{
+            con = db.createConnection();
+            pstmt = con.prepareStatement("select * from employee where dept=?");
+            pstmt.setString(1, dept);
+            res = pstmt.executeQuery();
+            while (res.next()) {
+                System.out.println("----------------------------------------------------------------");
+                System.out.println(
+                    "Id: " + res.getString("eid") + "\n" + 
+                    "Name: " + res.getString("name") + "\n" + 
+                    "Designation: " + res.getString("designation")  + "\n" + 
+                    "Department: "+ res.getString("dept")  + "\n" + 
+                    "Contact: " + res.getString("contact")
+                    );
+                    System.out.println("----------------------------------------------------------------");
+            }
+        }
+        catch(Exception e)
+        {
+            System.err.println(e);
+        } 
+    } 
+    public void removeById(int id){
+        Database db = new Database();
+        try{
+            con = db.createConnection();
+            pstmt = con.prepareStatement("delete from employee where eid = ?");
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+            System.out.println("Employee "+ "id "+" has been removed!\n----------------------------------------------------------------");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }        
+    }
 }
--- Hrms.java
+++ Hrms.java
@@ -1,14 +1,13 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Hrms {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {   
         Scanner sc = new Scanner(System.in);
         int choice, eid;
         String name, des, dept, contact;
         System.out.println("********************** WELCOME **********************");
         do{
-            System.out.print("1. Employee management\n2. Attendance management\n0. Exit\nEnter your choice: ");
+            System.out.print("1. Employee management\n2. Attendance management\n3. Leave management\n0. Exit\nEnter your choice: ");
             choice = sc.nextInt();
             switch(choice){
                 case 0:
@@ -17,7 +16,7 @@ public class Hrms {
                 case 1:
                     EmpMgmtService service = new EmpMgmtService();
                     do{
-                        System.out.print("1. Add Employee\n2. View All Employees\n3. Search Employee \n4. Remove employee\nEnter your choice: ");
+                        System.out.print("1. Add Employee\n2. View All Employees\n3. Search Employee \n4. Remove employee\n0. Go back\nEnter your choice: ");
                         choice = sc.nextInt();
                         switch(choice){
                             case 0:
@@ -45,7 +44,7 @@ public class Hrms {
                                 break;
                             case 3:
                                 do{
-                                    System.out.print("1.Search by EID\n2. Search by name\n3. Search by designation\n4. Search by department\n0. Exit\nEnter your choice: ");
+                                    System.out.print("1.Search by EID\n2. Search by name\n3. Search by designation\n4. Search by department\n0. Go back\nEnter your choice: ");
                                     choice = sc.nextInt();
                                     switch(choice){
                                         case 0: 
@@ -66,6 +65,9 @@ public class Hrms {
                                             service.searchByDesignation(des);
                                         break;
                                         case 4:
+                                        System.out.print("Enter designation to search employees: ");
+                                            dept = sc.next();
+                                            service.searchByDepartment(dept);
                                         break;
                                         default:
                                         System.out.println("Enter valid choice");
@@ -73,6 +75,11 @@ public class Hrms {
                                     }
                                 }while(choice!=0);
                                 choice = 1;
+                                break;
+                            case 4:
+                                System.out.print("Enter employee ID to remove: ");
+                                int id = sc.nextInt();
+                                service.removeById(id);
                                 break;
                             default: 
                                 System.out.println("Enter valid choice");
